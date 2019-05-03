e92702410d19ecef9c6c004fa166e5f6b45e8973
Techgawd <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2019-05-03 02:48:07 -0400

    Created one event listener, listed additional 9

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+//Fun Bus on Nav turns yellow with mouseover
+const funbus = document.querySelector (".logo-heading");
+
+funbus.addEventListener('mouseover', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('keydown', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('wheel', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('drag', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('load', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('focus', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('resize', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('scroll', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('select', (event) => { event.target.style.backgroundColor = 'yellow'; });
+
+funbus.addEventListener('dblclick', (event) => { event.target.style.backgroundColor = 'yellow'; });
